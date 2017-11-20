4ba855d1e55600b474458ce0a3c3eed2072bb729
radoslav.tomas <radoslav.tomas@gmail.com>
2017-11-20 15:45:00 +0000

    added bulma + buefy + font-awesome

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,13 +7,13 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
-
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * require Vue and Buefy
  */
+
+window.Vue = require('vue');
+import Buefy from 'buefy';
+Vue.use(Buefy);
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
